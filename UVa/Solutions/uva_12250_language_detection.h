5082e975a52ae4dcce243912591f993a467b4e4c
Tianyi <tianyiwang666@gmail.com>
2019-03-20 17:48:58 -0400

    12250 init

---
+++ UVa/Solutions/uva_12250_language_detection.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include "common.h"
+
+class UVA_12250
+{
+public:
+
+	void Solution()
+	{
+		
+	}
+
+	void Solution(const char* input, const char* output)
+	{
+		FILE *cfin = fopen(input, "r");
+		FILE *cfout = fopen(output, "w");
+
+		
+
+		fclose(cfin);
+		fclose(cfout);
+	}
+
+};
---
+++ UVa/uva_12250_language_detection_test.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+
+#include "Solutions\uva_12250_language_detection.h"
+
+
+TEST(UVA_12250, UVA_12250_Test)
+{
+	UVA_12250 uva_12250;
+
+	uva_12250.Solution("Tests/uva_12250_language_detection/input.txt", "Tests/uva_12250_language_detection/my_output.txt");
+}
