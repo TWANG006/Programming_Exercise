1aac6895575463f828f523a3375087f0d14ee49f
Tianyi <tianyiwang666@gmail.com>
2019-03-20 16:21:21 -0400

    11364 parking done

--- UVa/Solutions/uva_11364_parking.h
+++ UVa/Solutions/uva_11364_parking.h
@@ -8,7 +8,29 @@ public:
 
 	void Solution()
 	{
+		int tc;
+		int n;
+		int tmp;
+		int max_d = -1, min_d = 100;
 
+		scanf("%d", &tc);
+
+		while (tc--)
+		{
+			max_d = -1, min_d = 100;
+
+			scanf("%d", &n);
+			while (n--)
+			{
+				scanf("%d", &tmp);
+				if (tmp > max_d)
+					max_d = tmp;
+				if (tmp < min_d)
+					min_d = tmp;
+			}
+
+			printf("%d\n", 2 * (max_d - min_d));
+		}
 	}
 
 	void Solution(const char* input, const char* output)
@@ -16,7 +38,29 @@ public:
 		FILE *cfin = fopen(input, "r");
 		FILE *cfout = fopen(output, "w");
 
+		int tc;
+		int n;
+		int tmp;
+		int max_d = -1, min_d = 100;
 
+		fscanf(cfin, "%d", &tc);
+
+		while (tc--)
+		{
+			max_d = -1, min_d = 100;
+
+			fscanf(cfin, "%d", &n);
+			while (n--)
+			{
+				fscanf(cfin, "%d", &tmp);
+				if (tmp > max_d)
+					max_d = tmp;
+				if (tmp < min_d)
+					min_d = tmp;
+			}
+
+			fprintf(cfout, "%d\n", 2 * (max_d - min_d));
+		}
 
 		fclose(cfin);
 		fclose(cfout);
