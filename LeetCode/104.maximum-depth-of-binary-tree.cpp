2401e3e14cf06605cbf75636aff6d32ca2cbdd95
Tianyi <twang006@e.ntu.edu.sg>
2019-12-04 22:03:03 -0500

    104, 111 done

---
+++ LeetCode/104.maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=104 lang=cpp
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (62.80%)
+ * Likes:    1740
+ * Dislikes: 61
+ * Total Accepted:    637.4K
+ * Total Submissions: 1M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the
+ * root node down to the farthest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * return its depth = 3.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        // root is null, then depth = 0
+        if (root == NULL)
+            return 0;
+        
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+// @lc code=end
+
---
+++ LeetCode/111.minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=111 lang=cpp
+ *
+ * [111] Minimum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (36.14%)
+ * Likes:    924
+ * Dislikes: 525
+ * Total Accepted:    345.3K
+ * Total Submissions: 953.2K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its minimum depth.
+ * 
+ * The minimum depth is the number of nodes along the shortest path from the
+ * root node down to the nearest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * return its minimum depth = 2.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    int minDepth(TreeNode *root)
+    {
+        // root is null, then depth=0
+        if (root == NULL)
+            return 0;
+
+        // left & right children
+        int leftMin = minDepth(root->left);
+        int rightMin = minDepth(root->right);
+
+        return (leftMin == 0 || rightMin == 0) ? leftMin + rightMin + 1 : min(leftMin, rightMin) + 1;
+    }
+};
+// @lc code=end
