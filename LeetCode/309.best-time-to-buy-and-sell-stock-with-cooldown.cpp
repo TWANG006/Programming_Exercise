c58525832c03a54298bb33d75abe7e5d365927a9
Tianyi Wang <tianyiwang666@gmail.com>
2020-05-04 11:45:29 -0400

    309 done

--- LeetCode/309.best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ LeetCode/309.best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -5,11 +5,41 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int maxProfit(vector<int>& prices) {
-        
+    int maxProfit(vector<int> &prices)
+    {
+        auto n = prices.size();
+
+        if (n == 0)
+            return 0;
+
+        // O(n), O(1) solution
+        int buy = INT_MIN;
+        int sell_1 = 0, sell_2 = 0;
+
+        for (auto &i : prices)
+        {
+            buy = max(buy, sell_2 - i);
+            sell_2 = sell_1;
+            sell_1 = max(sell_1, buy + i);
+        }
+
+        return sell_1;
+
+        // O(n), O(n) solution
+        // vector<int> buy(n, 0);
+        // vector<int> sell(n, 0);
+        // buy[0] = -prices[0];
+
+        //        for (int i = 1; i < n; i++)
+        // {
+        //     sell[i] = max(sell[i - 1], buy[i - 1] + prices[i]);
+        //     buy[i] = max(buy[i - 1], i >= 2 ? sell[i - 2] - prices[i] : -prices[i]);
+        // }
+
+        // return sell[n - 1];        
     }
 };
 // @lc code=end
-
